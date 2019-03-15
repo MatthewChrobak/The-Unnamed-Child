9b46a3354921e83c0ed9bbfc21d3260b3c0b4e88
Matthew Chrobak <MatthewChrobak@hotmail.com>
2019-03-15 19:44:41 -0400

    Fixed event bugs + sample event

--- Game/Game/Events/EventQueue.cs
+++ Game/Game/Events/EventQueue.cs
@@ -7,7 +7,7 @@ namespace Game.Events
 {
     public class EventQueue : Singleton
     {
-        private List<GameEvent>[] queue = new List<GameEvent>[(int)PriorityTypes.END];
+        private List<GameEvent>[] queue = new List<GameEvent>[(int)PriorityTypes.END + 1];
 
         public EventQueue() {
             for (int i = (int)PriorityTypes.START; i <= (int)PriorityTypes.END; i++) {
@@ -19,14 +19,23 @@ namespace Game.Events
             queue[(int)type].Add(e);
         }
 
+        public void AddEvent(PriorityTypes type, Func<EVENT_RETURN> e, int interval_ms, int delay_ms) {
+            AddEvent(type, new GameEvent(e, interval_ms, delay_ms));
+        }
+
         public void Run() {
             int tick;
             int lastTick = Environment.TickCount;
 
             while (true) {
                 tick = Environment.TickCount;
-                int dif = lastTick - tick;
+                int dif = tick - lastTick;
+                lastTick = tick;
 
+                if (dif == 0) {
+                    Thread.Yield();
+                    continue;
+                }
                 for (int i = (int)PriorityTypes.START; i <= (int)PriorityTypes.END; i++) {
                     foreach (var e in queue[i]) {
                         e.Probe(dif);
--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -8,6 +8,10 @@ namespace Game
         public static void Main(string[] args) {
             var queue = Singleton.Create<EventQueue>();
 
+            // Placeholder for actual graphics.
+            queue.AddEvent(PriorityTypes.GRAPHICS, () => {
+                return EVENT_RETURN.NONE;
+            }, 16, 0);
 
 
             queue.Run();
