672cf73f1e7b1767f0faa2a47c21e5826a490acc
Matthew Chrobak <MatthewChrobak@hotmail.com>
2019-03-16 18:13:55 -0400

    Added environment objects

---
+++ Game/Game/Models/Rooms/Objects/Door.cs
@@ -0,0 +1,36 @@
+﻿using Game.Patterns.Singleton;
+using System;
+
+namespace Game.Models.Rooms.Objects
+{
+    [Serializable]
+    public class Door : CollisionObject
+    {
+        public string RoomName;
+        public float PlayerX;
+        public float PlayerY;
+
+        public override bool Probe(float x, float y) {
+            if (base.Probe(x, y)) {
+
+                var data = Singleton.Get<DataManager>();
+                data.LoadRoom(this.RoomName);
+
+                float playerx = PlayerX;
+                float playery = PlayerY;
+
+                data.Player.SetPos(playerx, playery);
+
+                return true;
+            }
+
+            return false;
+        }
+
+        public override void HandleAdditionalParamsForCreation(string[] cmd) {
+            this.RoomName = cmd[3];
+            this.PlayerX = float.Parse(cmd[4]);
+            this.PlayerY = float.Parse(cmd[5]);
+        }
+    }
+}
