7c0707150e44a7a043ea9ec7cf9dde79abad7b3c
theonlychunsta <lau_tsechun@hotmail.com>
2019-03-16 17:00:01 -0400

    merge

--- Game/Game/Character.cs
+++ Game/Game/Character.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿dusing System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -11,41 +11,47 @@ namespace Game
 {
     public class Program
     {
-        public static void Main(string[] args) {
+        public static void Main(string[] args)
+        {
             var queue = Singleton.Create<EventQueue>();
             var ui = Singleton.Create<UIManager>();
             var graphics = Singleton.Create<GameWindow>();
             var sound = Singleton.Create<SoundManager>();
 
-            
+            ui.LoadScene<MainMenu>();
+
             //PlaySound or PlayMusic; the second parameter is optional and sets the volume
             queue.AddEvent(PriorityTypes.SOUNDS, () =>
             {
                 sound.PlaySound(sound.footstepGrass1, 15f);
-                
+
                 return EVENT_RETURN.NONE;
             }, 1000, 0);
 
-            queue.AddEvent(PriorityTypes.SOUNDS, () =>
-            {
-                sound.PlaySound(sound.footstepGrass2, 0.01f);
 
             // Placeholder for actual graphics.
-            queue.AddEvent(PriorityTypes.GRAPHICS, () => {
+            queue.AddEvent(PriorityTypes.GRAPHICS, () =>
+            {
 
                 graphics.Draw();
                 return EVENT_RETURN.NONE;
             }, 16, 0);
-            queue.AddEvent(PriorityTypes.INPUT, () => {
+
+            queue.AddEvent(PriorityTypes.INPUT, () =>
+            {
 
                 Joystick.Update();
                 var scene = Singleton.Get<UIManager>().CurrentScene;
 
-                for (uint i = 0; i < 1; i++) {
-                    if (Joystick.IsConnected(i)) {
-                        for (uint ii = 0; ii < (uint)JoystickAxis.LENGTH; ii++) {
+                for (uint i = 0; i < 1; i++)
+                {
+                    if (Joystick.IsConnected(i))
+                    {
+                        for (uint ii = 0; ii < (uint)JoystickAxis.LENGTH; ii++)
+                        {
                             float position = Joystick.GetAxisPosition(i, (Joystick.Axis)ii);
-                            if (Math.Abs(position) >= 5) {
+                            if (Math.Abs(position) >= 5)
+                            {
                                 scene.JoystickMoved(i, (JoystickAxis)ii, position);
                             }
                         }
@@ -53,7 +59,11 @@ namespace Game
                 }
 
 
+                return EVENT_RETURN.NONE;
+            }, 16, 0);
+
             queue.Run();
+
         }
     }
 }
