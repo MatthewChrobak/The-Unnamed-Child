6de9e8e03061b684d61751ec16858d61394ac549
theonlychunsta <lau_tsechun@hotmail.com>
2019-03-15 21:10:49 -0400

    merge
    merge

--- Game/Game/Program.cs
+++ Game/Game/Program.cs
@@ -1,5 +1,6 @@
 ﻿using Game.Events;
 using Game.Patterns.Singleton;
+using Game.Sounds;
 
 namespace Game
 {
@@ -7,13 +8,17 @@ namespace Game
     {
         public static void Main(string[] args) {
             var queue = Singleton.Create<EventQueue>();
+            var sound = Singleton.Create<SoundManager>();
+
+
+            sound.getSound();
 
             // Placeholder for actual graphics.
             queue.AddEvent(PriorityTypes.GRAPHICS, () => {
                 return EVENT_RETURN.NONE;
             }, 16, 0);
 
-
+            
             queue.Run();
         }
     }
---
+++ Game/Game/Sounds/SoundManager.cs
@@ -0,0 +1,42 @@
+﻿using Game.Patterns.Singleton;
+using SFML.Audio;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+
+namespace Game.Sounds
+{
+    public class SoundManager: Singleton
+    {
+
+        public SoundBuffer buffer;
+        
+        public string getSound()
+        {
+            {
+                buffer->loadFromFile();
+                
+                Sound sound1 = "sound.wav";
+                sound1.setBuffer(buffer);
+                sound1.Play;
+
+
+                if (!buffer.loadFromFile("sound.wav"))
+                {
+
+
+                }
+
+                return sound1;
+             
+        }
+
+        public void useSound()
+        {
+            //sf::SoundBuffer buffer;
+        }
+    }
+}
