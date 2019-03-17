307bcf76ff6f408ce699ac2270067a63f6540fd9
darkheros12 <thephi.nguyen@mail.concordia.ca>
2019-03-17 13:30:16 -0400

    commit exit games

--- Game/Game/Models/Rooms/Objects/Exit.cs
+++ Game/Game/Models/Rooms/Objects/Exit.cs
@@ -11,11 +11,21 @@ namespace Game.Models.Rooms.Objects
                 var data = Singleton.Get<DataManager>();
                 var player = data.Player;
                 
-                if (!player.HasApron) {
+                if (!player.HasApron && player.hasClever) {
                     data.CurrentRoom.AddFloatingMessage("I can't just jump out of this house!", 200, y - 100, 3000);
                     return true;
                 }
-
+                if (!player.hasClever)
+                {
+                    data.CurrentRoom.AddFloatingMessage("Oh no... I am blocked by this wood plank!", 200, y - 100, 3000);
+                    return true;
+                }
+                if (player.hasClever)
+                {
+                    data.CurrentRoom.AddFloatingMessage("This is fun, it's finally open...", 200, y - 100, 3000);
+                    data.CurrentRoom.Objects[this.ItemID] = null;
+                    return true;
+                }
             }
 
             return true;
---
+++ Game/Game/UserInterface/Scenes/Credit.cs
@@ -0,0 +1,30 @@
+﻿using Game.Graphics;
+using Game.IO;
+using Game.Models;
+using Game.Models.Entities;
+using Game.Models.Rooms;
+using Game.Models.Rooms.Objects;
+using Game.Patterns.Singleton;
+using Game.UserInterface.Components;
+using SFML.Graphics;
+using System;
+namespace Game.UserInterface.Scenes
+{
+    public class Credit : Scene
+    {
+        public override void Draw(IDrawableSurface surface)
+        {
+            //TODO: draw the credit image
+            var data = Singleton.Get<DataManager>();
+            data.CurrentRoom.Draw(surface);
+            data.Player.Draw(surface);
+            data.BabaYaga?.Draw(surface);
+            base.Draw(surface);
+
+            surface.Draw("graphics/end_credit.png", new Graphics.Contexts.SurfaceContext()
+            {
+                Size = (GameWindow.WINDOW_WIDTH, GameWindow.WINDOW_HEIGHT)
+            });
+        }
+    }
+}
